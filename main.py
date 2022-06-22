b1f0cdf32780f4a05bacd5936954de4c2a1aa774
remont212 <107880286+remont212@users.noreply.github.com>
2022-06-22 10:14:11 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,18 +8,20 @@ from telebot import TeleBot
 import requests
 EURU = requests.get("https://free.currconv.com/api/v7/convert?q=EUR_RUB&compact=ultra&apiKey=626159f7b3598d14845d")
 USRU = requests.get("https://free.currconv.com/api/v7/convert?q=USD_RUB&compact=ultra&apiKey=626159f7b3598d14845d")
-
+CNRU = requests.get("https://free.currconv.com/api/v7/convert?q=CNY_RUB&compact=ultra&apiKey=626159f7b3598d14845d")
 
 bot = TeleBot("5420849865:AAEKnic88WTs3cy1obUgZlI4i2EeKglNwic")
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-  mess1 =  f'Hello, {message.from_user.first_name}! Чем могу помочь?'
-  markup = types.ReplyKeyboardMarkup()
+  mess1 =  f'Привет, {message.from_user.first_name}! Чем могу помочь?'
+  markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
   btn1 = types.KeyboardButton('Доллар = Рубль')
   btn2 = types.KeyboardButton('Евро = Рубль')
-  markup.add(btn1, btn2)
+  btn3 = types.KeyboardButton('Юань = Рубль')
+
+  markup.add(btn1, btn2, btn3)
   
   bot.send_message(message.chat.id, mess1, reply_markup=markup)
 
@@ -29,7 +31,10 @@ def dr(message):
      bot.send_message(message.chat.id, USRU.content)
   elif message.text == 'Евро = Рубль':
      bot.send_message(message.chat.id, EURU.content)
+  elif message.text == 'Юань = Рубль':
+     bot.send_message(message.chat.id, CNRU.content)
+  
   else:
     bot.send_message(message.chat.id, 'Error')
 
-bot.polling(non_stop=True)
+bot.polling(none_stop=True)
